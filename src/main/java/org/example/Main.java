e470c60768af869c3f9a65876904918b6fb17622
dokninski <ddokninski@gmail.com>
2023-03-04 20:46:25 +0100

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,11 @@ import java.nio.file.Paths;
 
 public class Main {
     private static final String menu =
-            "1. Podaj dwie daty aby wyświetlić nazwy filmów wydanych pomiędzy tymi datami\n" +
-                    "2. Wyświetl wszystkie informacje o losowym filmie\n" +
-                    "3. Podaj imię i nazwisko aktora aby wyświetlić nazwy filmów w jakich zagrał\n" +
-                    "4. Zakończ program";
+            """
+                    1. Podaj dwie daty aby wyświetlić nazwy filmów wydanych pomiędzy tymi datami
+                    2. Wyświetl wszystkie informacje o losowym filmie
+                    3. Podaj imię i nazwisko aktora aby wyświetlić nazwy filmów w jakich zagrał
+                    4. Zakończ program""";
 
     public static void main(String[] args) {
 
@@ -17,7 +18,7 @@ public class Main {
             InputDataHandler inputDataHandler = new InputDataHandler();
             Movie movie = new Movie();
             switch (inputDataHandler.getIntFromUser(menu)) {
-                case 1 -> movie.getFilmWithReleaseDateBetween(movieLibrary.getMoviesList(), inputDataHandler.getIntFromUser("rok od"), inputDataHandler.getIntFromUser("rok do"));
+                case 1 -> movie.getFilmWithReleaseDateBetween(movieLibrary.getMoviesList(), inputDataHandler.getIntFromUser("Rok od"), inputDataHandler.getIntFromUser("Rok do"));
                 case 2 -> movie.getAllInformationAboutRandomMovie(movieLibrary.getMoviesList());
                 case 3 -> movie.getMoviesWhereActorPlays(movieLibrary.getMoviesList(), inputDataHandler.getTextFromUser("Actor first name: "), inputDataHandler.getTextFromUser("Actor last name: "));
                 case 4 -> System.exit(0);
